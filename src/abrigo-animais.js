14c76294658b6457f6764e33e7d10ea97a9028c6
Thiago Sales <thiagosales46@gmail.com>
2025-09-09 22:06:24 -0300

    feat: new tests

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,17 +1,16 @@
 const ANIMAIS = new Map([
-  ['Rex', { especie: 'cão', brinquedos: ['RATO', 'BOLA'] }],
-  ['Mimi', { especie: 'gato', brinquedos: ['BOLA', 'LASER'] }],
-  ['Fofo', { especie: 'gato', brinquedos: ['BOLA', 'RATO', 'LASER'] }],
-  ['Zero', { especie: 'gato', brinquedos: ['RATO', 'BOLA'] }],
-  ['Bola', { especie: 'cão', brinquedos: ['CAIXA', 'NOVELO'] }],
-  ['Bebe', { especie: 'cão', brinquedos: ['LASER', 'RATO', 'BOLA'] }],
-  ['Loco', { especie: 'jabuti', brinquedos: ['SKATE', 'RATO'] }]
+  ["Rex", { especie: "cão", brinquedos: ["RATO", "BOLA"] }],
+  ["Mimi", { especie: "gato", brinquedos: ["BOLA", "LASER"] }],
+  ["Fofo", { especie: "gato", brinquedos: ["BOLA", "RATO", "LASER"] }],
+  ["Zero", { especie: "gato", brinquedos: ["RATO", "BOLA"] }],
+  ["Bola", { especie: "cão", brinquedos: ["CAIXA", "NOVELO"] }],
+  ["Bebe", { especie: "cão", brinquedos: ["LASER", "RATO", "BOLA"] }],
+  ["Loco", { especie: "jabuti", brinquedos: ["SKATE", "RATO"] }],
 ]);
 
 const MAX_ANIMAIS_POR_PESSOA = 3;
 
 class AbrigoAnimais {
-
   pessoaPodeAdotar(brinquedosPessoa, brinquedosAnimal) {
     if (brinquedosAnimal.length === 0) return true;
 
@@ -30,22 +29,34 @@ class AbrigoAnimais {
   pessoaPodeAdotarLoco(brinquedosPessoa, brinquedosLoco) {
     if (brinquedosLoco.length === 0) return true;
     const brinquedosPessoaSet = new Set(brinquedosPessoa);
-    return brinquedosLoco.every(brinquedo => brinquedosPessoaSet.has(brinquedo));
+    return brinquedosLoco.every((brinquedo) =>
+      brinquedosPessoaSet.has(brinquedo)
+    );
   }
 
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
-    const listaBrinquedosP1 = brinquedosPessoa1 === '' ? [] : brinquedosPessoa1.split(',').map(b => b.trim());
-    const listaBrinquedosP2 = brinquedosPessoa2 === '' ? [] : brinquedosPessoa2.split(',').map(b => b.trim());
-    const animaisParaConsiderar = ordemAnimais === '' ? [] : ordemAnimais.split(',').map(a => a.trim());
+    const listaBrinquedosP1 =
+      brinquedosPessoa1 === ""
+        ? []
+        : brinquedosPessoa1.split(",").map((b) => b.trim());
+    const listaBrinquedosP2 =
+      brinquedosPessoa2 === ""
+        ? []
+        : brinquedosPessoa2.split(",").map((b) => b.trim());
+    const animaisParaConsiderar =
+      ordemAnimais === "" ? [] : ordemAnimais.split(",").map((a) => a.trim());
 
-    if (new Set(listaBrinquedosP1).size !== listaBrinquedosP1.length || new Set(listaBrinquedosP2).size !== listaBrinquedosP2.length) {
-      return { erro: 'Brinquedo inválido' };
+    if (
+      new Set(listaBrinquedosP1).size !== listaBrinquedosP1.length ||
+      new Set(listaBrinquedosP2).size !== listaBrinquedosP2.length
+    ) {
+      return { erro: "Brinquedo inválido" };
     }
 
     const nomesAnimaisUnicos = new Set();
     for (const nomeAnimal of animaisParaConsiderar) {
       if (!ANIMAIS.has(nomeAnimal) || nomesAnimaisUnicos.has(nomeAnimal)) {
-        return { erro: 'Animal inválido' };
+        return { erro: "Animal inválido" };
       }
       nomesAnimaisUnicos.add(nomeAnimal);
     }
@@ -58,16 +69,30 @@ class AbrigoAnimais {
       const animal = ANIMAIS.get(nomeAnimal);
       let podeAdotarP1, podeAdotarP2;
 
-      if (nomeAnimal === 'Loco' && animaisParaConsiderar.length > 1) {
-        podeAdotarP1 = this.pessoaPodeAdotarLoco(listaBrinquedosP1, animal.brinquedos);
-        podeAdotarP2 = this.pessoaPodeAdotarLoco(listaBrinquedosP2, animal.brinquedos);
+      if (nomeAnimal === "Loco" && animaisParaConsiderar.length > 1) {
+        podeAdotarP1 = this.pessoaPodeAdotarLoco(
+          listaBrinquedosP1,
+          animal.brinquedos
+        );
+        podeAdotarP2 = this.pessoaPodeAdotarLoco(
+          listaBrinquedosP2,
+          animal.brinquedos
+        );
       } else {
-        podeAdotarP1 = this.pessoaPodeAdotar(listaBrinquedosP1, animal.brinquedos);
-        podeAdotarP2 = this.pessoaPodeAdotar(listaBrinquedosP2, animal.brinquedos);
+        podeAdotarP1 = this.pessoaPodeAdotar(
+          listaBrinquedosP1,
+          animal.brinquedos
+        );
+        podeAdotarP2 = this.pessoaPodeAdotar(
+          listaBrinquedosP2,
+          animal.brinquedos
+        );
       }
 
-      const pessoa1AtingiuLimite = adocoesPessoa1.length >= MAX_ANIMAIS_POR_PESSOA;
-      const pessoa2AtingiuLimite = adocoesPessoa2.length >= MAX_ANIMAIS_POR_PESSOA;
+      const pessoa1AtingiuLimite =
+        adocoesPessoa1.length >= MAX_ANIMAIS_POR_PESSOA;
+      const pessoa2AtingiuLimite =
+        adocoesPessoa2.length >= MAX_ANIMAIS_POR_PESSOA;
 
       const aptoP1 = podeAdotarP1 && !pessoa1AtingiuLimite;
       const aptoP2 = podeAdotarP2 && !pessoa2AtingiuLimite;
--- src/abrigo-animais.test.js
+++ src/abrigo-animais.test.js
@@ -1,31 +1,139 @@
 import { AbrigoAnimais } from "./abrigo-animais";
 
-describe('Abrigo de Animais', () => {
-
-  test('Deve rejeitar animal inválido', () => {
-    const resultado = new AbrigoAnimais().encontraPessoas('CAIXA,RATO', 'RATO,BOLA', 'Lulu');
-    expect(resultado.erro).toBe('Animal inválido');
+describe("Abrigo de Animais", () => {
+  test("Deve rejeitar animal inválido", () => {
+    const resultado = new AbrigoAnimais().encontraPessoas(
+      "CAIXA,RATO",
+      "RATO,BOLA",
+      "Lulu"
+    );
+    expect(resultado.erro).toBe("Animal inválido");
     expect(resultado.lista).toBeFalsy();
   });
 
-  test('Deve encontrar pessoa para um animal', () => {
+  test("Deve encontrar pessoa para um animal", () => {
     const resultado = new AbrigoAnimais().encontraPessoas(
-      'RATO,BOLA', 'RATO,NOVELO', 'Rex,Fofo');
-      expect(resultado.lista[0]).toBe('Fofo - abrigo');
-      expect(resultado.lista[1]).toBe('Rex - pessoa 1');
-      expect(resultado.lista.length).toBe(2);
-      expect(resultado.erro).toBeFalsy();
+      "RATO,BOLA",
+      "RATO,NOVELO",
+      "Rex,Fofo"
+    );
+    expect(resultado.lista[0]).toBe("Fofo - abrigo");
+    expect(resultado.lista[1]).toBe("Rex - pessoa 1");
+    expect(resultado.lista.length).toBe(2);
+    expect(resultado.erro).toBeFalsy();
   });
 
-  test('Deve encontrar pessoa para um animal intercalando brinquedos', () => {
-    const resultado = new AbrigoAnimais().encontraPessoas('BOLA,LASER',
-      'BOLA,NOVELO,RATO,LASER', 'Mimi,Fofo,Rex,Bola');
+  test("Deve encontrar pessoa para um animal intercalando brinquedos", () => {
+    const resultado = new AbrigoAnimais().encontraPessoas(
+      "BOLA,LASER",
+      "BOLA,NOVELO,RATO,LASER",
+      "Mimi,Fofo,Rex,Bola"
+    );
 
-      expect(resultado.lista[0]).toBe('Bola - abrigo');
-      expect(resultado.lista[1]).toBe('Fofo - pessoa 2');
-      expect(resultado.lista[2]).toBe('Mimi - abrigo');
-      expect(resultado.lista[3]).toBe('Rex - abrigo');
-      expect(resultado.lista.length).toBe(4);
-      expect(resultado.erro).toBeFalsy();
+    expect(resultado.lista[0]).toBe("Bola - abrigo");
+    expect(resultado.lista[1]).toBe("Fofo - pessoa 2");
+    expect(resultado.lista[2]).toBe("Mimi - abrigo");
+    expect(resultado.lista[3]).toBe("Rex - abrigo");
+    expect(resultado.lista.length).toBe(4);
+    expect(resultado.erro).toBeFalsy();
+  });
+
+  test("Deve rejeitar brinquedo duplicado", () => {
+    const resultado = new AbrigoAnimais().encontraPessoas(
+      "BOLA,RATO,BOLA",
+      "",
+      "Rex"
+    );
+    expect(resultado.erro).toBe("Brinquedo inválido");
+    expect(resultado.lista).toBeFalsy();
+  });
+
+  test("Deve aplicar regra do Loco quando houver companhia", () => {
+    const resultado = new AbrigoAnimais().encontraPessoas(
+      "RATO,SKATE",
+      "BOLA",
+      "Loco,Rex"
+    );
+    expect(resultado.lista[0]).toBe("Loco - pessoa 1");
+    expect(resultado.lista[1]).toBe("Rex - abrigo");
+    expect(resultado.erro).toBeFalsy();
+  });
+
+  test("NÃO deve aplicar regra do Loco quando não houver companhia", () => {
+    const resultado = new AbrigoAnimais().encontraPessoas(
+      "RATO,SKATE",
+      "",
+      "Loco"
+    );
+    expect(resultado.lista[0]).toBe("Loco - abrigo");
+    expect(resultado.erro).toBeFalsy();
+  });
+
+  test("Deve limitar a adoção a 3 animais por pessoa", () => {
+    const brinquedosPessoa1 = "LASER,RATO,BOLA,CAIXA,NOVELO";
+    const animais = "Bebe,Rex,Zero,Bola";
+    const resultado = new AbrigoAnimais().encontraPessoas(
+      brinquedosPessoa1,
+      "",
+      animais
+    );
+
+    const adocoesPessoa1 = resultado.lista.filter((r) =>
+      r.includes("pessoa 1")
+    ).length;
+    expect(adocoesPessoa1).toBe(3);
+
+    expect(resultado.lista).toContain("Bola - abrigo");
+    expect(resultado.erro).toBeFalsy();
+  });
+
+  test("Deve retornar uma lista vazia para entradas vazias", () => {
+    const resultado = new AbrigoAnimais().encontraPessoas("", "", "");
+    expect(resultado.lista).toEqual([]);
+    expect(resultado.erro).toBeFalsy();
+  });
+
+  test("Deve mandar para o abrigo quando ambos são aptos (empate)", () => {
+    const resultado = new AbrigoAnimais().encontraPessoas(
+      "RATO,BOLA",
+      "RATO,BOLA",
+      "Rex"
+    );
+    expect(resultado.lista[0]).toBe("Rex - abrigo");
+    expect(resultado.erro).toBeFalsy();
+  });
+
+  test("Deve mandar Loco para o abrigo quando ambos são aptos (empate)", () => {
+    const resultado = new AbrigoAnimais().encontraPessoas(
+      "RATO,SKATE",
+      "SKATE,RATO",
+      "Rex,Loco"
+    );
+    expect(resultado.lista[0]).toBe("Loco - abrigo");
+    expect(resultado.lista[1]).toBe("Rex - abrigo");
+    expect(resultado.erro).toBeFalsy();
+  });
+
+  test("Deve passar o animal para pessoa 2 se a pessoa 1 atingir o limite (O Herdeiro)", () => {
+    const brinquedosP1 = "LASER,RATO,BOLA,CAIXA,NOVELO";
+    const brinquedosP2 = "CAIXA,NOVELO";
+    const animais = "Bebe,Rex,Zero,Bola";
+
+    const resultado = new AbrigoAnimais().encontraPessoas(
+      brinquedosP1,
+      brinquedosP2,
+      animais
+    );
+
+    expect(resultado.lista).toContain("Bebe - pessoa 1");
+    expect(resultado.lista).toContain("Rex - pessoa 1");
+    expect(resultado.lista).toContain("Zero - pessoa 1");
+
+    expect(resultado.lista).toContain("Bola - pessoa 2");
+
+    const adocoesPessoa1 = resultado.lista.filter((r) =>
+      r.includes("pessoa 1")
+    ).length;
+    expect(adocoesPessoa1).toBe(3);
   });
 });
